420edd0e3597f41574fcee9b5930b33f1f891256
Antonio <bucchiarone@fbk.eu>
2022-09-21 15:49:24 +0200

    add the spinner

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <l-map v-if="showMap" :zoom="zoom" :center="center" style="height: 93vh">
+    <div class="loader" v-if="showSpinner"></div>
+    <l-map
+      v-if="showMap"
+      :zoom="zoom"
+      :center="[userLocation.lat, userLocation.lng]"
+      style="height: 93vh"
+    >
       <l-tile-layer :url="url" :attribution="attribution" />
 
       <l-control>
@@ -357,8 +363,8 @@ export default {
       showSmartHub: false,
       enableTooltip: true,
       showMap: false,
-      zoom: 14,
-      center: [46.07266104809106, 11.130024731304905],
+      showSpinner: true,
+      zoom: 16,
       MercatiGeoJson: null,
       CentralineGeoJson: null,
       FerrAriaJson: null,
@@ -714,6 +720,7 @@ export default {
             lat: pos.coords.latitude,
             lng: pos.coords.longitude,
           };
+          this.showSpinner = false;
           this.showMap = true;
         });
       } else {
@@ -903,6 +910,49 @@ button {
 }
 .mx-auto:hover {
   box-shadow: 9px 9px 9px rgba(20, 20, 20, 0.6);
+}
+
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+.loader {
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #08a1b3;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
 
